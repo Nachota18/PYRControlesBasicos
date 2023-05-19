1ec1b3c08f9cb0e606bd1ddf0b268e334fe2b05d
root <mgiraudo@ies21.edu.ar>
2023-05-19 12:53:53 -0300

    Agregar archivos de proyecto.

---
+++ PYSP1/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace PYSP1
+{
+    partial class lolI
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkVALO = new System.Windows.Forms.CheckBox();
+            this.chkLOLSITO = new System.Windows.Forms.CheckBox();
+            this.mrcSelecciondeSexo = new System.Windows.Forms.GroupBox();
+            this.cmdTOCAAQUI = new System.Windows.Forms.Button();
+            this.lblEscribir = new System.Windows.Forms.Label();
+            this.mrcSelecciondeSexo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chkVALO
+            // 
+            this.chkVALO.AutoSize = true;
+            this.chkVALO.Location = new System.Drawing.Point(23, 65);
+            this.chkVALO.Name = "chkVALO";
+            this.chkVALO.Size = new System.Drawing.Size(128, 43);
+            this.chkVALO.TabIndex = 0;
+            this.chkVALO.Text = "VALO";
+            this.chkVALO.UseVisualStyleBackColor = true;
+            // 
+            // chkLOLSITO
+            // 
+            this.chkLOLSITO.AutoSize = true;
+            this.chkLOLSITO.Location = new System.Drawing.Point(209, 65);
+            this.chkLOLSITO.Name = "chkLOLSITO";
+            this.chkLOLSITO.Size = new System.Drawing.Size(101, 43);
+            this.chkLOLSITO.TabIndex = 0;
+            this.chkLOLSITO.Text = "LOL";
+            this.chkLOLSITO.UseVisualStyleBackColor = true;
+            // 
+            // mrcSelecciondeSexo
+            // 
+            this.mrcSelecciondeSexo.Controls.Add(this.chkLOLSITO);
+            this.mrcSelecciondeSexo.Controls.Add(this.chkVALO);
+            this.mrcSelecciondeSexo.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mrcSelecciondeSexo.Location = new System.Drawing.Point(17, 47);
+            this.mrcSelecciondeSexo.Name = "mrcSelecciondeSexo";
+            this.mrcSelecciondeSexo.Size = new System.Drawing.Size(345, 118);
+            this.mrcSelecciondeSexo.TabIndex = 1;
+            this.mrcSelecciondeSexo.TabStop = false;
+            this.mrcSelecciondeSexo.Text = "Seleccion de Droga";
+            // 
+            // cmdTOCAAQUI
+            // 
+            this.cmdTOCAAQUI.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdTOCAAQUI.Location = new System.Drawing.Point(17, 204);
+            this.cmdTOCAAQUI.Name = "cmdTOCAAQUI";
+            this.cmdTOCAAQUI.Size = new System.Drawing.Size(294, 129);
+            this.cmdTOCAAQUI.TabIndex = 2;
+            this.cmdTOCAAQUI.Text = "TOCAAQUI";
+            this.cmdTOCAAQUI.UseVisualStyleBackColor = true;
+            this.cmdTOCAAQUI.Click += new System.EventHandler(this.cmdTOCAAQUI_Click);
+            // 
+            // lblEscribir
+            // 
+            this.lblEscribir.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEscribir.Location = new System.Drawing.Point(413, 66);
+            this.lblEscribir.Name = "lblEscribir";
+            this.lblEscribir.Size = new System.Drawing.Size(614, 210);
+            this.lblEscribir.TabIndex = 3;
+            this.lblEscribir.Click += new System.EventHandler(this.lblEscribir_Click);
+            // 
+            // lolI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1039, 450);
+            this.Controls.Add(this.lblEscribir);
+            this.Controls.Add(this.cmdTOCAAQUI);
+            this.Controls.Add(this.mrcSelecciondeSexo);
+            this.Name = "lolI";
+            this.Text = "TOCAAQUI";
+            this.Load += new System.EventHandler(this.lolI_Load);
+            this.mrcSelecciondeSexo.ResumeLayout(false);
+            this.mrcSelecciondeSexo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkVALO;
+        private System.Windows.Forms.CheckBox chkLOLSITO;
+        private System.Windows.Forms.GroupBox mrcSelecciondeSexo;
+        private System.Windows.Forms.Button cmdTOCAAQUI;
+        private System.Windows.Forms.Label lblEscribir;
+    }
+}
+
---
+++ PYSP1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PYSP1
+{
+    public partial class lolI : Form
+    {
+        public lolI()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdTOCAAQUI_Click(object sender, EventArgs e)
+        {
+            lblEscribir.Text = " ";
+            if (chkVALO.Checked == true)
+
+                lblEscribir.Text += "DEJA EL TINDER  ";
+
+
+             if (chkLOLSITO.Checked == true)
+
+
+                lblEscribir.Text += "TOCA PASTO FLACO ";
+        }
+
+        private void lolI_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblEscribir_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
